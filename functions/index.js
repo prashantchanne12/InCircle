7c2e29cdfb299537878ff797a752937d304fdfc2
prashantchanne12 <prashantchanne121@gmail.com>
2020-05-09 16:00:27 +0530

    Added firestore function to delete messages

--- functions/index.js
+++ functions/index.js
@@ -261,3 +261,43 @@ exports.onCreateActivityFeedItem = functions.firestore
     }
   });
 
+// ---------- onCreate Message ------------
+
+exports.onCreateMessage = functions.firestore
+    .document("/messages/{chatId}/chats/{docId}")
+    .onCreate(async (snapshot, context) => {
+
+        const chatId = context.params.chatId;
+        const docId = context.params.docId;
+
+
+        const userMessages = admin.firestore()
+                        .collection('messages')
+                        .doc(chatId)
+                        .collection('chats')
+                        .where('isSeen', '==', true);
+
+
+        const querySnapshot = await userMessages.get();
+
+
+        querySnapshot.forEach((doc) => {
+
+//            followerId = doc.id;
+
+            admin
+                .firestore()
+                .collection('messages')
+                .doc(chatId)
+                .collection('chats')
+                .doc(doc.id)
+                .get().then(doc => {
+
+                    if(doc.exists){
+                    setTimeout(doc.ref.delete(),25000);
+                    }
+
+                });
+        });
+});
+
